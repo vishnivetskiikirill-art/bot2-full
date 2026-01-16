57511629e4f0e6aff4660b7f4905f141a1845e74
vishnivetskiikirill-art <vishnivetskiikirill@gmail.com>
2026-01-16 11:08:05 +0200

    Update db.py

--- api/db.py
+++ api/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from .settings import settings
+from settings import settings
 
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 
