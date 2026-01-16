cd36893f0fdf20be1800835bd34cd4a31d2f61d7
vishnivetskiikirill-art <vishnivetskiikirill@gmail.com>
2026-01-16 12:20:20 +0200

    Update db.py

--- api/db.py
+++ api/db.py
@@ -1,9 +1,12 @@
+# api/db.py
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from settings import settings
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set in Railway Variables")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
