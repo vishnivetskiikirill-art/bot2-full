3d55dfe2d8618b8e09440f76cc9cf29b16d1a984
vishnivetskiikirill-art <vishnivetskiikirill@gmail.com>
2026-01-16 13:22:28 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+WEBAPP_DIR = BASE_DIR / "api" / "webapp"
+
+@app.get("/api/health")
+def health():
+    return {"status": "ok"}
+
+app.mount("/", StaticFiles(directory=WEBAPP_DIR, html=True), name="webapp")
