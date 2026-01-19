f1c62b9e0bee1f11d5f956817094c37abeb199ac
vishnivetskiikirill-art <vishnivetskiikirill@gmail.com>
2026-01-19 11:46:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-from fastapi import FastAPI
-from pathlib import Path
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-
-BASE_DIR = Path(__file__).resolve().parent
-WEBAPP_DIR = BASE_DIR / "api" / "webapp"
-
-@app.get("/api/health")
-def health():
-    return {"status": "ok"}
-
-# раздаём webapp как корень сайта
-app.mount("/", StaticFiles(directory=WEBAPP_DIR, html=True), name="webapp")
+from api.main import app
